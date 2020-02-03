7b0e4a8317d19a6c19ce336b172e09a1ad515a6d
Wallace <devwallace93@gmail.com>
2020-02-03 19:26:12 -0300

    Version 1.0

---
+++ src/components/Header/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
+import { MdShoppingBasket } from "react-icons/md";
+
+import { Container, Cart } from "./styles";
+
+import logo from "../../assets/images/images.png";
+
+function Header({ cartSize }) {
+  return (
+    <Container>
+      <Link to="/">
+        <div className="logo">
+          <img src={logo} alt="SanvMochilas" />
+          <h1> SANV </h1>
+        </div>
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong> Meu carrinho </strong>
+          <span>{cartSize} itens</span>
+        </div>
+
+        <MdShoppingBasket size={36} color="#fff" />
+      </Cart>
+    </Container>
+  );
+}
+
+export default connect(state => ({
+  cartSize: state.cart.length
+}))(Header);
