077160bcac8e9efd927a9756acc38dfe87f68231
Wallace <devwallace93@gmail.com>
2020-02-07 22:05:37 -0300

    Correção de erro do carrinho

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,25 @@
-import React, { useState } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import { MdAddShoppingCart } from "react-icons/md";
-import { formatPrice, useEffect } from "../../util/format";
+import { formatPrice } from "../../util/format";
 import api from "../../services/api";
 
 import * as CartActions from "../../store/modules/cart/actions";
 
 import { ProductList } from "./styles";
 
-function Home({ amount, addToCartRequest }) {
+export default function Home() {
   const [products, setProducts] = useState([]);
+  const amount = useSelector(state =>
+    state.cart.reduce((sumAmount, product) => {
+      sumAmount[product.id] = product.amount;
+
+      return sumAmount;
+    }, {})
+  );
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     async function loadProducts() {
@@ -20,14 +29,15 @@ function Home({ amount, addToCartRequest }) {
         ...product,
         priceFormatted: formatPrice(product.price)
       }));
+
+      setProducts(data);
     }
 
-    setProducts(data);
     loadProducts();
   }, []);
 
   function handleAddProduct(id) {
-    addToCartRequest(id);
+    dispatch(CartActions.addToCartRequest(id));
   }
 
   return (
@@ -37,10 +47,7 @@ function Home({ amount, addToCartRequest }) {
           <img src={product.image} alt={product.title} />
           <strong>{product.title}</strong>
           <span>{product.priceFormatted}</span>
-          <button
-            type="button"
-            onClick={() => this.handleAddProduct(product.id)}
-          >
+          <button type="button" onClick={() => handleAddProduct(product.id)}>
             <div>
               <MdAddShoppingCart size={16} color="#fff" />
               {""}
@@ -53,16 +60,3 @@ function Home({ amount, addToCartRequest }) {
     </ProductList>
   );
 }
-
-const mapStateToProps = state => ({
-  amount: state.cart.reduce((amount, product) => {
-    amount[product.id] = product.amount;
-
-    return amount;
-  }, {})
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(CartActions, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
